cfee737419a63cdefad002628aa774ae5adbac7d
rayhan <rayhanmahmud990099@gmail.com>
2023-11-07 21:07:29 +0600

    done delete

--- index.js
+++ index.js
@@ -113,6 +113,14 @@ async function run() {
             res.send(result)
         })
 
+        app.delete('/jobs/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) };
+            const result = await jobsCollection.deleteOne(query)
+            console.log(result);
+            res.send(result)
+        })
+
 
 
         // Send a ping to confirm a successful connection
